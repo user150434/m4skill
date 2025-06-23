c66f7901dd8098b6dcdac86cb241a2173e17cd6c
user150434 <o.boudaati@gmail.com>
2025-06-23 12:56:45 +0200

    fixed and finished

--- onepager/js/main.js
+++ onepager/js/main.js
@@ -23,7 +23,7 @@ sections.forEach(section => {
 const contactForm = document.querySelector('.contact__form');
 contactForm.addEventListener('submit', async (e) => {
     e.preventDefault();
-    
+
     const formData = new FormData(contactForm);
     const data = {
         name: formData.get('name'),
@@ -52,3 +52,22 @@ contactForm.addEventListener('submit', async (e) => {
         console.error('Error:', error);
     }
 });
+
+
+
+
+
+function sendMail(e) {
+
+    const formulier = document.querySelector('.contact__form');
+    const mailtoValue = formulier.querySelector('input[name="send_to_mail"]').value;
+    const mailCCValue = formulier.querySelector('input[name="email"]').value;
+    const naam = formulier.querySelector('input[name="name"]').value;
+    const bericht = formulier.querySelector('textarea[name="message"]').value;
+
+    const subject = `Bericht van ${encodeURIComponent(naam)}`;
+    const body = `Beste: ${encodeURIComponent(naam)} (${encodeURIComponent(mailCCValue)})%0D%0A%0D%0ABericht:%0D%0A${encodeURIComponent(bericht)}`;
+
+    const mailtoLink = `mailto:${mailtoValue}?cc=${mailCCValue}&subject=${subject}&body=${body}`;
+    window.location.href = mailtoLink;
+}
